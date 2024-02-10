e6394b6d9f4e67a4c9e70e4d306f0c9f6293c0d3
Daniil Akutsionak <116372042+DaniilAkutsionak@users.noreply.github.com>
2024-02-10 17:06:40 +0300

    New features

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import './App.css';
 import Accordian from './components/accordian';
+import ImageSlider from './components/image-slider';
+import RandomColor from './components/random-color';
+import StarRating from './components/star-rating';
 
 function App() {
   return (
    <div className='App'>
-    <Accordian></Accordian>
+    {/*<Accordian></Accordian>*/}
+    {/*<RandomColor></RandomColor>*/}
+   {/*<StarRating noOfStars={10}></StarRating>*/}
+   <ImageSlider url={'https://picsum.photos/v2/list'} page={'1'} limit={'4'}></ImageSlider>
    </div>
   );
 }
