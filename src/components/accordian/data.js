b5d52a3fb2966a67d19171c280be27c993622ec8
Daniil Akutsionak <116372042+DaniilAkutsionak@users.noreply.github.com>
2024-02-10 14:14:42 +0300

    f

---
+++ src/components/accordian/data.js
@@ -0,0 +1,25 @@
+const data = [
+    {
+        id: '1',
+        question: "What teh accordion components?",
+        answer:'sdfssdsdffds',
+    },
+    {
+        id: '2',
+        question: "Are ypu my master?",
+        answer:'No',
+    },
+    {
+        id: '3',
+        question: "Hello?",
+        answer:'Hi',
+    },
+    {
+        id: '4',
+        question: "I don'n care about this question?",
+        answer:"fsfsfsd"
+    },
+];
+
+export default data;
+    
